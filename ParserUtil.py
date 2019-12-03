f7f86ff2e061b9b581ba623e6194ea1b211f71a6
Chief Nelson <chief@Chiefs-MacBook-Pro.local>
2019-12-02 21:26:07 -0500

    Added more parser utility

--- ParserUtil.py
+++ ParserUtil.py
@@ -17,3 +17,55 @@ def tokensLeft():
     return len(tokenList)
 
 
+#peek the first x elements 
+def peek(x):
+
+    l = []
+
+    for i in range(0, x):
+
+        l.append(tokenList[i])
+
+    return l
+
+
+
+def peekUntil(stop):
+
+    l = []
+    i = 0
+
+    while not stop(tokenList[i]):
+
+        l.append(tokenList[i])
+        i += 1
+
+    return l
+
+    
+
+#pop the firt x elements 
+def pop(x):
+
+    l = []
+
+    for i in range(0, x):
+
+        l.append(tokenList.pop(0))
+
+    return l
+
+
+
+def popUntil(stop):
+
+    l = []
+
+    while not stop(tokenList[0]):
+
+        l.append(tokenList.pop(0))
+
+    return l
+
+
+
