bea3064e7c01cfa83826af123f5a5b47b1285d18
Chief Nelson <chief@Chiefs-MacBook-Pro.local>
2019-12-03 00:08:16 -0500

    Added statement class

--- Parser.py
+++ Parser.py
@@ -6,6 +6,31 @@ statementList = []
 line = 1 
 
 
+#possible kinds of statements:
+#"var_assign"
+#"expression"
+#"function_dec"
+#"function_call"
+#"conditional"
+#"loop"
+#
+#also any kind of TokenEntry can also be a kind of statement
+
+
+class Statement:
+
+    def __init__(self, kind, subList=[]):
+
+        self.kind = kind
+        self.subList = subList
+
+    def addToken(tokenEntry):
+
+        subList.append(Statement(tokenEntry.kind, [tokenEntry.token]))
+    
+
+
+
 def parseStatement():
 
     #handle variable declarations and assignments
@@ -15,11 +40,13 @@ def parseStatement():
 
 
 
-def parseVarAssignment()
+def parseVarAssignment():
+
+    varAssignment = Statement("var_assign")
 
     if utl.peek().token in mtl.types:
 
-        
+        varAssignment.addToken(utl.pop())
 
 
 
