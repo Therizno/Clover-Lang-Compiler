6763b669fe66901b64516779bb949152a58bd13f
Chief Nelson <chief@Chiefs-MacBook-Pro.local>
2019-12-07 21:50:30 -0500

    Iterator progress

--- ContextizerUtil.py
+++ ContextizerUtil.py
@@ -8,6 +8,26 @@ def parse(fileName):
 
     statmList = par.parse(fileName)
 
+    iterate()
+
 
 
 # Statement data structure iterator 
+
+def iterate():
+
+    for item in statmList:
+
+        iterateRecursive(item)
+
+
+
+def iterateRecursive(state):
+
+    global nextStatement
+
+    for item in state.subList:
+
+        if isinstance(item, str):
+
+            
